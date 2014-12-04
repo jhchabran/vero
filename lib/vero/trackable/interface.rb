1c43b0b4b46f5f3c22a7ca4119d1c3f85e3f08bc
Jean-Hadrien Chabran <jh@chabran.fr>
2014-12-04 12:33:22 +0000

    Add missing interfaces methods

--- lib/vero/trackable/interface.rb
+++ lib/vero/trackable/interface.rb
@@ -16,6 +16,18 @@ module Vero
       def unsubscribe!
         with_default_vero_context.unsubscribe!
       end
+
+      def reidentify! previous_id
+        with_default_vero_context.reidentify! previoud_id
+      end
+
+      def update_user!
+        with_default_vero_context.update_user!
+      end
+
+      def update_user_tags!(add = [], remove = [])
+        with_default_vero_context.update_user_tags! add, remove
+      end
     end
   end
 end
